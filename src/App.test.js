1e1cab2140843b9f5a7cc3de5858e722aaf47839
CryptoRUSHGav <info@cryptorush.app>
2021-12-10 16:24:22 -0500

    update ticker

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders call to action', () => {
+test("renders call to action", () => {
   render(<App />);
-  const linkElement = screen.getByText(/GET \$RSHCP/i);
+  const linkElement = screen.getByText(/GET \$RUSHCOPR/i);
   expect(linkElement).toBeInTheDocument();
 });
